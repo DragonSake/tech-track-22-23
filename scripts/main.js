dea09edb2e49a360e78590c78ff0f392731d4644
DragonSake <40611000+DragonSake@users.noreply.github.com>
2022-12-01 16:44:21 +0100

    test

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ import "../styles/style.css";
 // We can use node_modules directely in the browser!
 import * as d3 from "d3";
 
-fetch("public/lol.json")
+fetch("lol.json")
   .then((response) => response.json())
   .then((dataSet) => {
     gold(dataSet), kills(dataSet), dmg(dataSet);
