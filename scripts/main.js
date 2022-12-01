b3320e0e6715130fa95c13a607becfb645b498a7
DragonSake <40611000+DragonSake@users.noreply.github.com>
2022-12-01 17:54:24 +0100

    Definitive

--- scripts/main.js
+++ scripts/main.js
@@ -4,27 +4,32 @@ import "../styles/style.css";
 // We can use node_modules directely in the browser!
 import * as d3 from "d3";
 
+// (api)data importeren
 fetch("lol.json")
   .then((response) => response.json())
   .then((dataSet) => {
-    gold(dataSet), kills(dataSet), dmg(dataSet);
+    gold(dataSet), kills(dataSet), dmg(dataSet); //functie uitvoeren
   });
 
+// functie maken + de lengte en breedte bepalen
 function gold(dataSet) {
   const chartWidth = 400;
   const chartHeight = 160;
 
+  // X-as
   const xScale = d3
     .scaleLinear()
     .domain([0, d3.max(dataSet, (d) => d.Gold)])
     .range([0, chartWidth]);
 
+  // Y-as
   const yScale = d3
     .scaleBand()
     .domain(d3.map(dataSet, (d) => d.Player))
     .range([0, chartHeight])
     .paddingInner(0.6);
 
+  //maakt de bar aan
   const bars = d3.select("#bars").selectAll("g").data(dataSet).join("g");
 
   bars
@@ -32,22 +37,24 @@ function gold(dataSet) {
     .attr("height", yScale.bandwidth())
     .attr("width", (d) => xScale(d.Gold))
     .attr("y", (d) => yScale(d.Player))
-    .attr("rx", 7);
+    .attr("rx", 7); //border-radius
 
   bars
     .append("text")
     .attr("y", (d) => yScale(d.Player) + yScale.bandwidth() / 2)
-    .attr("x", (d) => xScale(d.Gold) + 40)
-    .text((d) => d.Gold);
+    .attr("x", (d) => xScale(d.Gold) + 40) //40= ruimte tussen het getal en bar
+    .text((d) => d.Gold); //haalt het getal uit de json
 
+  //namen van de spelers
   d3.select("#labels")
     .selectAll("text")
     .data(dataSet)
     .join("text")
     .attr("y", (d) => yScale(d.Player) + yScale.bandwidth() / 2)
-    .text((d) => d.Player);
+    .text((d) => d.Player); //haalt de naam uit de json
 }
 
+// data van de vision score
 const data1 = [
   { group: "Zeus", value: 1 },
   { group: "Oner", value: 1.75 },
@@ -56,6 +63,7 @@ const data1 = [
   { group: "Keria", value: 2.36 },
 ];
 
+// data van de kda
 const data2 = [
   { group: "Zeus", value: 3.2 },
   { group: "Oner", value: 4.5 },
@@ -64,12 +72,13 @@ const data2 = [
   { group: "Keria", value: 7.6 },
 ];
 
+//bepaalt de lengte en breedte
 const margin = { top: 30, right: 30, bottom: 70, left: 60 },
   width = 300 - margin.left - margin.right,
   height = 450 - margin.top - margin.bottom;
 
 const svg = d3
-  .select(".my_dataviz")
+  .select(".my_dataviz") //selecteert de div met de class my_dataviz
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -111,9 +120,9 @@ function update(data) {
 
   svg.select(".myYaxis").call(d3.axisLeft(newY));
 
-  svg.selectAll(".domain").attr("stroke", "white");
+  svg.selectAll(".domain").attr("stroke", "white"); //maakt de lijn wit
 
-  svg.selectAll(".tick").select("line").attr("stroke", "white");
+  svg.selectAll(".tick").select("line").attr("stroke", "white"); //maakt de streepjes wit
 
   const u = svg.selectAll("rect").data(data);
 
@@ -132,15 +141,17 @@ function update(data) {
     .attr("height", (d) => {
       return height - newY(d.value);
     })
-    .attr("fill", "#03bdc9");
+    .attr("fill", "#03bdc9"); //geeft kleur
 }
 
+// functie "update" uitvoeren
 update(data1);
 
+// selecteert de buttons
 const button1 = document.querySelector("button");
+const button2 = document.querySelector("button:nth-child(3)");
 
-const button2 = document.querySelector("button:nth-child(2)");
-
+// voegt een click toe aan de button en voert een functie uit
 button1.addEventListener("click", function () {
   update(data1);
 });
@@ -149,19 +160,17 @@ button2.addEventListener("click", function () {
   update(data2);
 });
 
+// maakt een functie aan en haalt de data van de 2e speler in de array
 function kills(dataSet, player = 2) {
-  console.log("kill data", dataSet[player]);
-
   let pieData = [dataSet[player].Kill, 100 - dataSet[player].Kill];
 
-  var text = "";
+  const text = "";
+  const width = 180;
+  const height = 180;
+  const thickness = 20;
+  const radius = Math.min(width, height) / 2;
 
-  var width = 180;
-  var height = 180;
-  var thickness = 20;
-
-  var radius = Math.min(width, height) / 2;
-
+  // functie eerste waarde wordt geel, tweede waarde wordt donkerblauw
   function color(d, i) {
     if (i === 0) {
       return "#e7bd3b";
@@ -170,30 +179,32 @@ function kills(dataSet, player = 2) {
     }
   }
 
-  var svg = d3
+  // selecteert de div met de class donut
+  const svg = d3
     .select(".donut")
     .append("svg")
     .attr("class", "pie")
     .attr("width", width)
     .attr("height", height);
 
-  var g = svg
+  const g = svg
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var arc = d3
+  // cirkel in de donut
+  const arc = d3
     .arc()
     .innerRadius(radius - thickness)
     .outerRadius(radius);
 
-  var pie = d3
+  const pie = d3
     .pie()
     .value(function (d) {
       return d;
     })
     .sort(null);
 
-  var path = g
+  const path = g
     .selectAll("path")
     .data(pie(pieData))
     .enter()
@@ -204,8 +215,6 @@ function kills(dataSet, player = 2) {
     .each(function (d, i) {
       this._current = i;
     });
-
-  g.append("text").attr("text-anchor", "middle").attr("dy", ".35em").text(text);
 
   svg
     .append("svg:text")
@@ -216,19 +225,17 @@ function kills(dataSet, player = 2) {
     .text(dataSet[player].Kill);
 }
 
+// maakt een functie aan en haalt de data van de 2e speler in de array
 function dmg(dataSet, player = 2) {
-  console.log("damage data", dataSet[player]);
-
   let pieData = [dataSet[player].Damage, 100 - dataSet[player].Damage];
 
-  var text = "";
+  const text = "";
+  const width = 180;
+  const height = 180;
+  const thickness = 20;
+  const radius = Math.min(width, height) / 2;
 
-  var width = 180;
-  var height = 180;
-  var thickness = 20;
-
-  var radius = Math.min(width, height) / 2;
-
+  // functie eerste waarde wordt geel, tweede waarde wordt donkerblauw
   function color(d, i) {
     if (i === 0) {
       return "#e7bd3b";
@@ -237,30 +244,32 @@ function dmg(dataSet, player = 2) {
     }
   }
 
-  var svg = d3
+  // selecteert de div met de class donut
+  const svg = d3
     .select(".donut2")
     .append("svg")
     .attr("class", "pie")
     .attr("width", width)
     .attr("height", height);
 
-  var g = svg
+  const g = svg
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var arc = d3
+  // cirkel in de donut
+  const arc = d3
     .arc()
     .innerRadius(radius - thickness)
     .outerRadius(radius);
 
-  var pie = d3
+  const pie = d3
     .pie()
     .value(function (d) {
       return d;
     })
     .sort(null);
 
-  var path = g
+  const path = g
     .selectAll("path")
     .data(pie(pieData))
     .enter()
@@ -271,8 +280,6 @@ function dmg(dataSet, player = 2) {
     .each(function (d, i) {
       this._current = i;
     });
-
-  g.append("text").attr("text-anchor", "middle").attr("dy", ".35em").text(text);
 
   svg
     .append("svg:text")
